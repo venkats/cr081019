f1e30d7335a89e05897b7d891f3c850116e0d188
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-08-10 10:41:27 +0530

    post lab

---
+++ exercise_1/Sample.java
@@ -0,0 +1,19 @@
+class Sample {    
+  public static boolean isPrime(int number) {
+    boolean divisible = false;
+    for(int i = 2; i < number; i++) {
+      if(number % i == 0) {
+        divisible = true;
+        break;
+      }
+    }         
+    
+    return number > 1 && !divisible;
+  } 
+  
+  public static void main(String[] args) {
+    for(int i = 1; i < 8; i++) {
+      System.out.printf("isPrime(%d)? %b\n", i, isPrime(i));
+    }
+  }              
+}
