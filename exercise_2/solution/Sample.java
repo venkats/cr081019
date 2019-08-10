7f70da46aeb2b49f6be18c7729e1921a256d049b
Venkat Subramaniam <venkats@agiledeveloper.com>
2019-08-10 11:39:30 +0530

    post solution

---
+++ exercise_2/solution/Sample.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.nio.file.*;
+
+class Sample {    
+  public static void main(String[] args) {
+    try {
+      String searchWord = "localhost";
+      String path = "/etc/hosts";
+      
+      long count = 
+        Files.lines(Paths.get(path))
+          .filter(line -> line.contains(searchWord))
+          .count();
+
+      System.out.printf("The word %s occured %d times\n", searchWord, count);
+    } catch(IOException ex) {
+      System.out.println("ERROR: " + ex.getMessage());
+    }
+  }              
+}
